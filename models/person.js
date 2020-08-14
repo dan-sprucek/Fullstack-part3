9b21974c2ba3e4e1d5f6044d89b73e406a482900
dan-sprucek <dan.sprucek@gmail.com>
2020-08-14 18:28:16 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const url = 'mongodb+srv://fullstack:qwer1234@cluster0.dlsm1.mongodb.net/phonebook-app?retryWrites=true&w=majority'
+const url = process.env.MONGODB_URI
 
 console.log("connecting to", url)
 
