a269d8afa503652db6e3d24ae21530a74e87bb34
dan-sprucek <dan.sprucek@gmail.com>
2020-08-14 23:12:29 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ require('dotenv').config()
 const Person = require("./models/person")
 const morgan = require("morgan")
 
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+  }
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  
 app.use(express.json())
 app.use(express.static("build"))
 
@@ -13,7 +29,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :m
 app.get('/', (req, res) => {
     res.send(`<h1>Hello</h1>`)
 })
-
+    
 app.get('/api/persons', (req, res) => {
     Person.find({}).then(persons => {
         res.json(persons)
@@ -34,14 +50,14 @@ app.get('/api/persons/:id', (req, res) => {
     })
 })
 
-// app.delete('/api/persons/:id', (req, res) => {
-//     const id = req.params.id
-//     console.log(id)
-    // persons = persons.filter(person => person.id !== id)
-    // Person.findById(req.params.id).then(person => {
-
-//     res.status(204).end()
-// })
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id).then(result => {
+        res.status(204).end()
+    })
+    .catch(error => {
+        next(error)
+    })
+})
     
 app.post('/api/persons', (req, res) => {
     const body = req.body
@@ -70,6 +86,9 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
+app.use(unknownEndpoint)
+app.use(errorHandler)
+    
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
